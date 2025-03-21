08e455d5bc861f116f72a1f9920af8a6fba6d622
Andi Shobash <andishubash@gmail.com>
2025-03-21 09:02:49 +0200

    test

--- devops/ci/webhook_server.py
+++ devops/ci/webhook_server.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 GIT_REPO = "https://github.com/ShobashAndi/gan_shmuel"
 
-LOCAL_REPO_PATH = "/home/andishobash/Desktop/gam_shmuel_test"
+LOCAL_REPO_PATH = "/app/gan_shmuel"
 
 SERVICES = ["weight", "billing"]
 
