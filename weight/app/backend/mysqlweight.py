64a1da578fc8c7685c466c07dcae162675ed06ca
Andi Shobash <andishubash@gmail.com>
2025-03-22 10:06:35 +0200

    test

--- weight/app/backend/mysqlweight.py
+++ weight/app/backend/mysqlweight.py
@@ -6,11 +6,11 @@ def connect():
     connection = None
     try:
         connection = mysql.connector.connect(
-            host=os.environ.get('MYSQL_HOST', 'weight_db_test'),
+            host=os.environ.get('MYSQL_HOST', 'localhost'),
             port=3306,
             user=os.environ.get('MYSQL_USER', 'root'),
             password=os.environ.get('MYSQL_PASSWORD', 'rootpassword'),
-            database=os.environ.get('MYSQL_DATABASE', 'weight'),
+            database="weight"
         )
         print("MySQL Database connection successful")
     except mysql.connector.Error as err:
